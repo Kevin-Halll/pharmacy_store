6878b27509ec71ce61653f4f328a58c899fe1177
Kevin-Halll <kev4improved@gmail.com>
2022-12-31 13:44:01 -0500

    cart item cards

--- lib/screens/cart-screen.dart
+++ lib/screens/cart-screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:pharmacy_store/utils/colors.dart';
 import 'package:pharmacy_store/utils/text.dart';
 
@@ -28,18 +29,116 @@ class Cart extends StatelessWidget {
             ),
           ),
         ),
+        centerTitle: true,
+        title: TextLG(text: "My Cart", color: Paints.darkGrey,),
       ),
 
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                TextMD(text: "8 items in cart"),
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: Row(
+                  children: [
+                    TextMD(text: "8 items in cart"),
 
-              ],
-            )
-          ],
+                  ],
+                ),
+              ),
+              SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      height: 110,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black, width: 2),
+                      ),
+                      child: Stack(
+                        children: [
+                          GestureDetector(
+                             onTap: (){
+                                print("you tried to delete");
+                             },
+                            child: Row(
+                               mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Column(
+                                  children: [
+                                    Expanded(
+                                      child: SizedBox(
+                                         width: 100,
+                                        child: Material(
+                                           elevation: 1,
+                                          color: Paints.primaryColor,
+                                          borderRadius: BorderRadius.only(topRight: Radius.circular(15), bottomRight: Radius.circular(15)),
+                                          // child: Icon(Icons.delete),
+                                          child: Row(
+                                            mainAxisAlignment: MainAxisAlignment.end,
+                                            children: [
+                                              Padding(
+                                                padding: const EdgeInsets.all(15.0),
+                                                child: Center(child: FaIcon(FontAwesomeIcons.trashCan, color: Colors.white, size: 15,)),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.8,
+                            child: Material(
+                              borderRadius: BorderRadius.circular(15),
+                              // color: Paints.darkGrey,
+                              elevation: 1,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(
+                                  children: [
+                                    Image.asset("assets/images/soles.png"),
+                                    Column(
+                                      children: [
+                                        Row(
+                                          mainAxisAlignment: MainAxisAlignment.start,
+                                          children: [
+                                            TextMD(text: "Orthopedic Soles", color: Paints.darkGrey,),
+                                          ],
+                                        ),
+                                        Row(
+                                          mainAxisAlignment: MainAxisAlignment.start,
+                                          children: [
+                                            SizedBox(
+                                              width: 60,
+
+                                              child: Divider(
+                                                color: Paints.secondaryColor,
+                                                thickness: 3,
+                                              ),
+                                            ),
+                                          ],
+                                        )
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     ));
